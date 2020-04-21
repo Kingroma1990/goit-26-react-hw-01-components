b1464af5acb2d01b9700fa02a2634663f186255a
Roma <king_roma@ukr.net>
2020-04-21 18:45:46 +0300

    Updates

---
+++ precache-manifest.fb26a3eb02d5f461b4aca788b9f9c13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae33696582285ef1963aa2738f545b5",
+    "url": "/goit-26-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "f488d3b4925dea9a26dd",
+    "url": "/goit-26-react-hw-01-components/static/css/main.54adf276.chunk.css"
+  },
+  {
+    "revision": "386a9f4a9e3eea391ea7",
+    "url": "/goit-26-react-hw-01-components/static/js/2.74ba57de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-26-react-hw-01-components/static/js/2.74ba57de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f488d3b4925dea9a26dd",
+    "url": "/goit-26-react-hw-01-components/static/js/main.034bdfc0.chunk.js"
+  },
+  {
+    "revision": "37e9a704203437ff9f73",
+    "url": "/goit-26-react-hw-01-components/static/js/runtime-main.7128828e.js"
+  }
+]);
